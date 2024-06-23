390e02802b64e2f53d3525c40e0225cf5ba308ff
Michael Ghen <mike@mikeghen.com>
2024-06-23 15:45:42 -0400

    anvil deploy script

--- script/DeployTestnet.s.sol
+++ script/DeployTestnet.s.sol
@@ -1,25 +1,24 @@
-// deployClubPoolFactory.s.sol
+// script/DeployTestnet.s.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import "../src/ClubPoolFactory.sol";
-import "../src/mocks/MockUSDC.sol";
-
-// Usage:
-// 
-// forge script script/DeployTestnet.s.sol --broadcast --rpc-url http://127.0.0.1:8545 --revert-on-error
+import "../src/Mocks/MockUSDC.sol";
 
 contract DeployTestnet is Script {
     function run() external {
         uint256 ownerKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         uint256 runnerKey = vm.envUint("RUNNER_PRIVATE_KEY");
-        address runner = address(uint160(uint256(keccak256(abi.encodePacked(runnerKey)))));
 
-        // Owner deloys the club pool
+        // Owner deploys the club pool
         vm.startBroadcast(ownerKey);
         MockUSDC usdc = new MockUSDC();
+        console.log("MockUSDC deployed at:", address(usdc));
+
         ClubPool clubPool = new ClubPool(address(usdc), 12 weeks, address(this), 50 * 1e6);
+        console.log("ClubPool deployed at:", address(clubPool));
+
         usdc.mint(0x70997970C51812dc3A010C7d01b50e0d17dc79C8, 100 * 1e6);
         vm.stopBroadcast();
 
@@ -28,5 +27,8 @@ contract DeployTestnet is Script {
         usdc.approve(address(clubPool), 50 * 1e6);
         clubPool.join();
         vm.stopBroadcast();
+
+        // Log final state
+        console.log("Runner joined the ClubPool with address:", address(clubPool));
     }
 }
